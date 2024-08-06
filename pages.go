469c7b7d9645b8540c6bc02e1c2e6f84b9afae38
fiatjaf <fiatjaf@gmail.com>
2024-08-05 21:44:14 -0300

    search box working.

--- pages.go
+++ pages.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func handleHomePage(w http.ResponseWriter, r *http.Request) {
@@ -18,4 +19,26 @@ func handleAppPage(w http.ResponseWriter, r *http.Request) {
 	}
 
 	app(platforms, def).Render(r.Context(), w)
+}
+
+type AppDefinition struct {
+	Name        string   `yaml:"name"`
+	Npub        string   `yaml:"npub"`
+	Categories  []string `yaml:"categories"`
+	Description string   `yaml:"description"`
+	Gallery     []string `yaml:"gallery"`
+	Features    []string `yaml:"features"`
+	Platforms   []string `yaml:"platforms"`
+	Source      string   `yaml:"source"`
+	Thumb       string   `yaml:"thumb"`
+	URL         string   `yaml:"url"`
+}
+
+func (a AppDefinition) Fulltext() string {
+	return strings.Join([]string{
+		a.Name,
+		a.Description,
+		a.URL,
+		a.Source,
+	}, " ")
 }
